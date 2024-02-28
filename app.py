ff0928f81bdff038bc0125b96ae019c7aabadab3
MacDiarmide <vasily2gavrilov@yandex.ru>
2024-02-28 13:32:50 +0300

    Russian added, Celsius added, app ready

--- app.py
+++ app.py
@@ -2,13 +2,18 @@ import requests
 
 api_key = 'ad6e3d1262d3f8dc675a19aaa2ee7702'
 
-user_input = input('Введите название города: ')
+while True:
+    user_input = input('Введите название города: ')
+    try:
+        weather_data = requests.get(
+            f'http://api.openweathermap.org/data/2.5/weather?q={user_input}&units=metric&lang=ru&appid={api_key}')
 
-# a Response object:
-weather_data = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={user_input}&appid={api_key}')
+        city_name = weather_data.json()['name']
+        weather = weather_data.json()['weather'][0]['description']
+        temp = round(weather_data.json()['main']['temp'])
 
-weather = weather_data.json()['weather'][0]['main']
-temp = round(weather_data.json()['main']['temp'])
+        print(f'Погода в городе {city_name}:\n'
+              f'{weather.capitalize()}, температура {temp}°C\n')
 
-print(f'Погода в {user_input}: {weather}\n'
-      f'Температура в {user_input} {temp} градусов Цельсия')
+    except:
+        print("Некорректное название города. Попробуйте еще раз, пожалуйста\n")
